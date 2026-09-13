98fd522570597775f46d2578029d517d67f2d9b1
Chandan Borole <chandanborole@gmail.com>
2026-09-13 10:50:52 +0530

    csslocators
    csslocators

---
+++ test_csslocators.py
@@ -0,0 +1,27 @@
+from playwright.sync_api import Page , expect
+
+def test_tagandid(page:Page):
+    # tag and id
+    page.goto("https://demowebshop.tricentis.com/")
+    page.locator("input#small-searchterms").fill("mobiles")
+    page.wait_for_timeout(5000)
+
+
+def test_tagandclass(page: Page):
+    # tag and class
+    page.goto("https://demowebshop.tricentis.com/")
+    page.locator("input.search-box-text").fill("mobiles")
+    page.wait_for_timeout(5000)
+
+
+def test_tagandattribute(page:Page):
+    # tag and attribute
+    page.goto("https://demowebshop.tricentis.com/")
+    page.locator("input[name=q]").fill("mobiles")
+    page.wait_for_timeout(5000)
+
+def test_tagclassandattribute(page:Page):
+    # tag , class and attribute
+    page.goto("https://demowebshop.tricentis.com/")
+    page.locator("input.search-box-text[name=q]").fill("mobiles")
+    page.wait_for_timeout(5000)
