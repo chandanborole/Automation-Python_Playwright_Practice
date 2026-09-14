a19465483e61181558f6dd42479797fd40f5b3d5
Chandan Borole <chandanborole@gmail.com>
2026-09-14 19:13:31 +0530

    dropdowns
    dropdowns

--- test_dropdowns.py
+++ test_dropdowns.py
@@ -1,25 +1,25 @@
 from playwright.sync_api import Page , expect
 
 def test_singleselect_dropdown(page:Page):
-  page.goto("https://testautomationpractice.blogspot.com/")
+    page.goto("https://testautomationpractice.blogspot.com/")
 
-  # 3 ways to select option from the dropdown
-  # select_option is a built-in method used to interact with and select one or more options inside standard HTML <select> dropdown elements
+    # 3 ways to select option from the dropdown
+    # select_option is a built-in method used to interact with and select one or more options inside standard HTML <select> dropdown elements
 
-  # 1. by label
-  page.locator("#country").select_option("India")
-  # OR
-  page.locator("#country").select_option(label="India")
+    # 1. by label
+    page.locator("#country").select_option("India")
+    # OR
+    page.locator("#country").select_option(label="India")
 
-  # 2. by value
-  page.locator("#country").select_option("germany")
-  # OR
-  page.locator("#country").select_option(value="germany")
+    # 2. by value
+    page.locator("#country").select_option("germany")
+    # OR
+    page.locator("#country").select_option(value="germany")
 
-  # 3. by index (starts from 0) (in single_select_dropdown we have to explicitly provide index keyword)
-  page.locator("#country").select_option(index=3)
+    # 3. by index (starts from 0) (in single_select_dropdown we have to explicitly provide index keyword)
+    page.locator("#country").select_option(index=3)
 
-  page.wait_for_timeout(5000)
+    page.wait_for_timeout(5000)
 
 
 def test_multiselect_dropdown(page:Page):
